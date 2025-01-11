8d24ee9512d397059420982feb403ede641636ce
charanjit <c.panesar15@gmail.com>
2025-01-12 00:47:22 +0530

    setup 3 sec

--- src/app/solutions/page.js
+++ src/app/solutions/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Solution_banner from '../components/solution_banner'
 import Solution_about_us from '../components/Solution_about_us'
+import Solution_transform from '../components/Solution_transform'
 
 const Solutions = () => {
   return (
     <>
         <Solution_banner />
         <Solution_about_us/>
+        <Solution_transform/>
     </>
   )
 }
