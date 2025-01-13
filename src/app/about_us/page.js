d2305adcee15bc7abc0d3be245eff8c6ad16bb68
charanjit <c.panesar15@gmail.com>
2025-01-13 21:32:32 +0530

    about setup

---
+++ src/app/about_us/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import About_banner from '../components/About_banner'
+
+const About_us = () => {
+    return (
+        <>
+            <About_banner/>
+        </>
+    )
+}
+
+export default About_us
