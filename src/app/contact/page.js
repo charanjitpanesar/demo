3deee28f14507f07ebf2d0f01eedd2f7c806b6f1
charanjit <c.panesar15@gmail.com>
2025-01-18 07:40:40 +0530

    contact banner

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Solution_calender from '../components/Solution_calender'
 import Scroll_star from '../components/scroll_star'
+import Contact_banner from '../components/Contact_banner'
 
 const page = () => {
     return (
         <>
             <div className="section" id='section'>
+                <Contact_banner/>
                 <Solution_calender/>
             </div>
             <Scroll_star />
