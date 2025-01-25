39489b334c7c974173a4ded2650f333fac43633e
ns5516517 <63893259+ns5516517@users.noreply.github.com>
2025-01-25 23:23:17 +0530

    modal done

--- src/app/components/Solution_calender.js
+++ src/app/components/Solution_calender.js
@@ -10,6 +10,7 @@ import meeting from '../../../public/images/meet.png';
 import user from '../../../public/images/user.png';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
+import Main_modal from './modal';
 const Calendar = dynamic(() => import('react-calendar'), {
     ssr: false,
     loading: () => <p style={{ color: 'black', fontSize: 14, fontWeight: 400 }}>Calender is Loading...</p>
@@ -19,9 +20,10 @@ const TimePicker = dynamic(() => import('react-time-picker'), {
     loading: () => <p style={{ color: 'black', fontSize: 14, fontWeight: 400 }}>Time is Loading...</p>
 })
 
-const Solution_calender = ({id}) => {
+const Solution_calender = ({ id }) => {
     const [active, setActive] = useState(0);
     const [val, setVal] = useState('');
+    const [show, setShow] = useState(false);
 
     const cardData = [
         {
@@ -52,12 +54,16 @@ const Solution_calender = ({id}) => {
         setActive(i)
     }
 
+    function handleshow(){
+        setShow(true)
+    }
+
     useEffect(() => {
         const inputGroup = document.querySelector('.react-time-picker__inputGroup');
         if (!inputGroup) return;
-    
+
         const select = inputGroup.childNodes;
-    
+
         if (val === '24h') {
             select[select.length - 1].style.display = 'none';
             select[select.length - 2].style.display = 'none';
@@ -65,130 +71,134 @@ const Solution_calender = ({id}) => {
             select[select.length - 1].style.display = 'block';
             select[select.length - 2].style.display = 'block';
         }
-    
+
         return () => {
             if (inputGroup) {
                 select[select.length - 1].style.display = 'block';
                 select[select.length - 2].style.display = 'block';
             }
+            setShow(false)
         };
     }, [val]);
-    
+
     return (
-        <section className='solution_calender_section' id={id}>
-            <Container>
-                <Row>
-                    <Col xxl={12} xl={12} lg={12} md={12} sm={12} xs={12}>
-                        <div className='parent_area'>
-                            <div className='upper_area'>
-                                <h3>Identify Your Needs</h3>
-                                <p className='green'>Let us know what challenges you’re facing.</p>
-                                <p>Whether it’s fleet optimization, compliance, or cost reduction, we’ll help you pinpoint areas for improvement.
-                                    <br></br>Why it matters: You get personalized advice tailored to your business</p>
-                            </div>
-                            <div className='cards_area'>
-                                <div className='arrow_img1'>
-                                    <Image src={arrowDown1} alt='...' title="..." priority></Image>
+        <>
+            <section className='solution_calender_section' id={id}>
+                <Container>
+                    <Row>
+                        <Col xxl={12} xl={12} lg={12} md={12} sm={12} xs={12}>
+                            <div className='parent_area'>
+                                <div className='upper_area'>
+                                    <h3>Identify Your Needs</h3>
+                                    <p className='green'>Let us know what challenges you’re facing.</p>
+                                    <p>Whether it’s fleet optimization, compliance, or cost reduction, we’ll help you pinpoint areas for improvement.
+                                        <br></br>Why it matters: You get personalized advice tailored to your business</p>
                                 </div>
-                                <div className='arrow_img2'>
-                                    <Image src={arrowDown2} alt='...' title="..." priority></Image>
-                                </div>
-                                {cardData.map((card, index) => (
-                                    <div
-                                        className={`card_inner ${index % 2 !== 0 ? 'even' : ''}`}
-                                        key={index}
-                                    >
-                                        <div className='circle_area'>{card.step}</div>
-                                        <div className='content_area'>
-                                            <h3>{card.title}</h3>
-                                            <p>{card.description}</p>
-                                        </div>
+                                <div className='cards_area'>
+                                    <div className='arrow_img1'>
+                                        <Image src={arrowDown1} alt='...' title="..." priority></Image>
                                     </div>
-                                ))}
-
-                            </div>
-                            <Form action="">
-                                <div className="calender_area">
-                                    <Row className='row-gap-4'>
-                                        <Col xxl={3} xl={3} lg={6} md={6} sm={6} xs={12} className='order-sm-1 order-1'>
-                                            <div className="left">
-                                                <div className="top">
-                                                    <div className="img_area">
-                                                        <Image src={user} alt='...' title='...' fetchPriority='low' priority={false} width={46} height={46} />
-                                                    </div>
-                                                    <div className="user_name">Muhammad Aqeel</div>
-                                                    <div className="meet">20 Minutes Meeting</div>
-                                                </div>
-                                                <div className="bottom">
-                                                    <ul>
-                                                        {
-                                                            meet &&
-                                                            meet.map((item, i) => (
-                                                                <li key={i}>
-                                                                    <div className="left_meet">
-                                                                        <Image src={item.src} alt='...' title='...' fetchPriority='low' priority={false} width={20} height={20} />
-                                                                    </div>
-                                                                    <div className="right_meet">{item.txt}</div>
-                                                                </li>
-                                                            ))
-                                                        }
-                                                    </ul>
-                                                </div>
+                                    <div className='arrow_img2'>
+                                        <Image src={arrowDown2} alt='...' title="..." priority></Image>
+                                    </div>
+                                    {cardData.map((card, index) => (
+                                        <div
+                                            className={`card_inner ${index % 2 !== 0 ? 'even' : ''}`}
+                                            key={index}
+                                        >
+                                            <div className='circle_area'>{card.step}</div>
+                                            <div className='content_area'>
+                                                <h3>{card.title}</h3>
+                                                <p>{card.description}</p>
                                             </div>
-                                        </Col>
-                                        <Col xxl={6} xl={6} lg={12} md={12} sm={12} xs={12} className='order-xl-1 order-sm-3 '>
-                                            <div className="center ">
-                                                <Calendar
-                                                    className={'cal'}
-                                                    tileClassName={'tile'}
+                                        </div>
+                                    ))}
 
-                                                />
-                                            </div>
-                                        </Col>
-                                        <Col xxl={3} xl={3} lg={6} md={6} sm={6} xs={12} className='order-sm-2 order-0'>
-                                            <div className="right ">
-                                                <div className='inner_right'>
+                                </div>
+                                <Form action="" onSubmit={(e) => e.preventDefault()}>
+                                    <div className="calender_area">
+                                        <Row className='row-gap-4'>
+                                            <Col xxl={3} xl={3} lg={6} md={6} sm={6} xs={12} className='order-sm-1 order-1'>
+                                                <div className="left">
                                                     <div className="top">
-                                                        <div className="left_clock">Wed <span>04</span></div>
-                                                        <div className="right_clock">
-                                                            <div className="format">
-                                                                {
-                                                                    ['12h', '24h'].map((item, i) => (
-                                                                        <div
-                                                                            key={i}
-                                                                            onClick={(e) => handleActive(i, e)}
-                                                                            className={`box ${active == i ? 'active' : ''}`}
-                                                                        >
-                                                                            {item}
+                                                        <div className="img_area">
+                                                            <Image src={user} alt='...' title='...' fetchPriority='low' priority={false} width={46} height={46} />
+                                                        </div>
+                                                        <div className="user_name">Muhammad Aqeel</div>
+                                                        <div className="meet">20 Minutes Meeting</div>
+                                                    </div>
+                                                    <div className="bottom">
+                                                        <ul>
+                                                            {
+                                                                meet &&
+                                                                meet.map((item, i) => (
+                                                                    <li key={i}>
+                                                                        <div className="left_meet">
+                                                                            <Image src={item.src} alt='...' title='...' fetchPriority='low' priority={false} width={20} height={20} />
                                                                         </div>
-                                                                    ))
-                                                                }
+                                                                        <div className="right_meet">{item.txt}</div>
+                                                                    </li>
+                                                                ))
+                                                            }
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </Col>
+                                            <Col xxl={6} xl={6} lg={12} md={12} sm={12} xs={12} className='order-xl-1 order-sm-3 '>
+                                                <div className="center ">
+                                                    <Calendar
+                                                        className={'cal'}
+                                                        tileClassName={'tile'}
+
+                                                    />
+                                                </div>
+                                            </Col>
+                                            <Col xxl={3} xl={3} lg={6} md={6} sm={6} xs={12} className='order-sm-2 order-0'>
+                                                <div className="right ">
+                                                    <div className='inner_right'>
+                                                        <div className="top">
+                                                            <div className="left_clock">Wed <span>04</span></div>
+                                                            <div className="right_clock">
+                                                                <div className="format">
+                                                                    {
+                                                                        ['12h', '24h'].map((item, i) => (
+                                                                            <div
+                                                                                key={i}
+                                                                                onClick={(e) => handleActive(i, e)}
+                                                                                className={`box ${active == i ? 'active' : ''}`}
+                                                                            >
+                                                                                {item}
+                                                                            </div>
+                                                                        ))
+                                                                    }
+                                                                </div>
                                                             </div>
                                                         </div>
+                                                        {/* <div className="time">{active == 0 ? '12:00 am' : '11:59'}</div> */}
+                                                        <div className="time">
+                                                            <TimePicker
+                                                                disableClock={true}
+                                                                autoFocus={false}
+                                                                value={new Date(2025, 0, 0, 10, 50)}
+                                                                format={val == '24h' ? 'H-m-a' : 'h-m-a'}
+                                                            />
+                                                        </div>
                                                     </div>
-                                                    {/* <div className="time">{active == 0 ? '12:00 am' : '11:59'}</div> */}
-                                                    <div className="time">
-                                                        <TimePicker
-                                                            disableClock={true}
-                                                            autoFocus={false}
-                                                            value={new Date(2025, 0, 0, 10, 50)}
-                                                            format={val == '24h'? 'H-m-a': 'h-m-a'}
-                                                        />
+                                                    <div className="button_area">
+                                                        <button className='btn-primary btn-black' onClick={handleshow}>GET STARTED</button>
                                                     </div>
                                                 </div>
-                                                <div className="button_area">
-                                                    <button  className=' btn-primary btn-black'>GET STARTED</button>
-                                                </div>
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                </div>
-                            </Form>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                </Form>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            <Main_modal show={show} setShow={setShow}/>
+        </>
     )
 }
 
---
+++ src/app/components/modal.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Button, Form, Modal } from 'react-bootstrap'
+import '../../../public/sass/pages/modal.scss';
+
+const Main_modal = ({ show, setShow }) => {
+    return (
+        <>
+            <Modal show={show} onHide={() => setShow(false)} backdrop="static" centered>
+                <Modal.Header>
+                    <div className="icon" onClick={() => setShow(false)}>
+                        <i className="bi bi-x-lg"></i>
+                    </div>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form action=''>
+                        <Form.Group controlId='email' className='form-group'>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type='email' placeholder='Enter your Email' name='email' aria-label='email' autoComplete='email' />
+                        </Form.Group>
+                        <Form.Group controlId='phonenumber' className='form-group'>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type='tel' placeholder='Enter your Mobile No.' name='phonenumber' aria-label='phonenumber' autoComplete='tel' />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button className='btn btn-primary btn-green' onClick={() => setShow(false)}>
+                        Submit
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default Main_modal
