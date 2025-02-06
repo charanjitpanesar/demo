a6d4d52db5840c0777591b1a9223cd25dab047e4
Aman <ak669212@gmail.com>
2025-02-06 22:09:53 +0530

    changs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    env: {
+        SMTP_USER: process.env.SMTP_USER,
+        SMTP_PASS: process.env.SMTP_PASS,
+        SMTP_EMAIL: process.env.SMTP_EMAIL,
+        TO_EMAIL: process.env.TO_EMAIL,
+    }
 };
 
 export default nextConfig;
