93610581047ed13b3e3f3e13abf9d0386ea8cd59
Aman <ak669212@gmail.com>
2025-02-06 21:13:58 +0530

    env fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        BASE_URL: process.env.BASE_URL,
+        MONGO_URI: process.env.MONGO_URI,
+        SALT_ROUNDS: process.env.SALT_ROUNDS,
+        SALT_SECRET: process.env.SALT_SECRET,
+        SMTP_USER: process.env.SMTP_USER,
+        SMTP_PASS: process.env.SMTP_PASS,
+        SMTP_EMAIL: process.env.SMTP_EMAIL,
+    }
 };
 
 export default nextConfig;
